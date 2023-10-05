a3ad4dcb14732a9a09d66ffe0d08ffdffdbc2961
Łukasz Michalak <l.michalak.firma@gmail.com>
2023-10-05 21:58:29 +0200

    fix

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,18 +1,20 @@
-import { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export const Modal = ({ src, alt, closeModal }) => {
-  const handleClose = e => {
+  const handleClose = useRef(e => {
     if (e.key === 'Escape') {
       closeModal();
     }
-  };
+  });
+
   useEffect(() => {
-    document.addEventListener('keydown', handleClose);
+    const handleKeyPress = e => handleClose.current(e);
+    document.addEventListener('keydown', handleKeyPress);
 
     return () => {
-      document.removeEventListener('keydown', handleClose);
+      document.removeEventListener('keydown', handleKeyPress);
     };
-  }, [handleClose]);
+  }, [closeModal]);
 
   const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
